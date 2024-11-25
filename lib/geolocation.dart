44eb9f98cda54525ed37f5ed46483c83ed9faf2a
zkyy484 <dzakymusyaddad@gmail.com>
2024-11-25 11:25:50 +0700

    praktikum 7

--- lib/geolocation.dart
+++ lib/geolocation.dart
@@ -9,48 +9,58 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+  Future<Position>? position;
+
+
   String myPosition = '';
 
   @override
   void initState() {
     super.initState();
-    getPosition().then((Position myPos) {
-      myPosition = 'Latitude: ${myPos.latitude.toString()} - Longitude: ${myPos.latitude.toString()}';
-      setState(() {
-        myPosition = myPosition;
-      });
-    });
+    position = getPosition();
+
+
+    // getPosition().then((Position myPos) {
+    //   myPosition = 'Latitude: ${myPos.latitude.toString()} - Longitude: ${myPos.latitude.toString()}';
+    //   setState(() {
+    //     myPosition = myPosition;
+    //   });
+    // });
   }
 
-
+  // Praktikum 7
   @override
   Widget build(BuildContext context) {
-    final myWidget = myPosition == ''
-    ? const CircularProgressIndicator()
-    : Text(myPosition);
-    
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Moch. Dzaky Musyaddad'),
+        title: Text('Current Location'),
       ),
-      body: Center(
-        child: Text(myPosition),
-      ),
+      body: Center(child: FutureBuilder(
+        future: position, 
+        builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const Text('Something terrible happened!');
+          }
+          else {
+            return Text(snapshot.data.toString());            
+          }
+        }),),
     );
   }
 
+  // Praktikum 6
   // @override
   // Widget build(BuildContext context) {
   //   final myWidget = myPosition == ''
   //   ? const CircularProgressIndicator()
-  //   : const Text(myPosition);;
-
+  //   : Text(myPosition);
+    
   //   return Scaffold(
   //     appBar: AppBar(
-  //       title: Text('Current Location'),
+  //       title: const Text('Moch. Dzaky Musyaddad'),
   //     ),
-  //     body:  Center(
-  //       child: myWidget,
+  //     body: Center(
+  //       child: Text(myPosition),
   //     ),
   //   );
   // }
