3b51f40edec3cac2b109a314abf22fcb03cbeea3
zkyy484 <dzakymusyaddad@gmail.com>
2024-11-25 09:14:37 +0700

    praktikum 2

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,31 @@ class FuturePage extends StatefulWidget {
 
 class _FuturePageState extends State<FuturePage> {
 
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
+  }
+
   Future<Response> getData() async {
     const authority = 'www.googleapis.com';
     const path = '/books/v1/volumes/MWW8DwAAQBAJ';
@@ -55,15 +80,18 @@ class _FuturePageState extends State<FuturePage> {
             ElevatedButton(
               child: const Text('GO!'),
               onPressed: () {
-                setState(() {});
-                getData()
-                .then((value) {
-                  result = value.body.toString().substring(0, 450);
-                  setState(() {});
-                }).catchError((_) {
-                  result = 'An error occured';
-                  setState(() {});
-                });
+                count();
+
+
+                // setState(() {});
+                // getData()
+                // .then((value) {
+                //   result = value.body.toString().substring(0, 450);
+                //   setState(() {});
+                // }).catchError((_) {
+                //   result = 'An error occured';
+                //   setState(() {});
+                // });
                 }),
               const Spacer(),
               Text(result),
@@ -76,6 +104,3 @@ class _FuturePageState extends State<FuturePage> {
     );
   }
 }
-
-
-
